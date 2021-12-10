cab405979df045aeb10ce06cfe3aa2a815a38df8
Dyllehyrton Xavier <dyllehyrton@gmail.com>
2021-12-10 08:23:11 -0300

    alteracoes necessarias

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,10 +3,10 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
-public class Pr imeiroPrograma {
+public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-        Livros livros = new Livros();
+        Livros livros = new Livros("O problema dos 3 corpos", 300);
 
         System.out.println(gato);
         System.out.println(livros);
@@ -19,5 +19,34 @@ public class Pr imeiroPrograma {
 
 class Livros {
     private String nome;
-    private String npg;
+    private Integer npg;
+
+    public Livros(String nome, Integer npg) {
+        this.nome = nome;
+        this.npg = npg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpg() {
+        return npg;
+    }
+
+    public void setNpg(Integer npg) {
+        this.npg = npg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", npg=" + npg +
+                '}';
+    }
 }
