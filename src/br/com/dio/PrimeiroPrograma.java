703c7e4e278ef7eb30bdb7bb7033df2dc7bbf0b3
Dyllehyrton Xavier <dyllehyrton@gmail.com>
2021-12-10 08:26:18 -0300

    alteracoes necessarias

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,9 +11,9 @@ public class PrimeiroPrograma {
         System.out.println(gato);
         System.out.println(livros);
 
-        /*int a = 5;
+        int a = 5;
         int b = 3;
-        System.out.println("Hello World!!" + (a+b));*/
+        System.out.println("Hello World!!" + (a+b));
     }
 }
 
